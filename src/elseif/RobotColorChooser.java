d813ced66fa1e304b27bed3e396731bd7f654f44
ANYA1128 <league@iMac.attlocal.net>
2018-05-04 18:30:14 -0700

    CrazyCatLady, HighLowGame, and RobotColorChooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,46 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		for (int i = 0; i < 10; i++) {
+			Robot rob = new Robot();
+			String answer = JOptionPane.showInputDialog(null, "What color would you like the robot to draw with?");
+			if (answer.equals("red")) {
+				rob.setPenColor(110, 0, 0);
+			} else if (answer.equals("green")) {
+				rob.setPenColor(0, 110, 0);
+			} else if (answer.equals("teal")) {
+				rob.setPenColor(0, 90, 100);
+			} else if (answer.equals("blue")) {
+				rob.setPenColor(0, 0, 110);
+			} else if (answer.equals("purple")) {
+				rob.setPenColor(110, 0, 110);
+			} else {
+				JOptionPane.showMessageDialog(null, "This color is not available");
+				rob.setRandomPenColor();
+			}
+				
+					
+				
+				rob.setSpeed(10000000);
+			    rob.setPenWidth(10);
+				rob.penDown();
+				rob.move(100);
+				rob.turn(90);
+				rob.move(100);
+				rob.turn(90);
+				rob.move(100);
+				rob.turn(90);
+				rob.move(100);
+				rob.turn(90);
 
-        //6. If the user doesn’t enter anything, choose a random color
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+			}
+		}
 	}
-}
+
