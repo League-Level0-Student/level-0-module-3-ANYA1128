1f5a5cc28d7411f50ab260474cc8b238dce99ecf
blackbeltcam <league@iMac.attlocal.net>
2018-05-18 18:31:57 -0700

    Birthday Reminder, Years Alive,and processing stuff

---
+++ src/apple_thingy/apple_thingy.pde
@@ -0,0 +1,20 @@
+void setup() {
+    size(500, 500);
+}
+void draw() {
+    background(200, 200, 200);
+    noStroke();
+fill(#EA0C2A);
+    
+    ellipse(150, 200, 150, 150);
+    ellipse(212, 200, 150, 150);
+    fill(#32935D);
+    rect(176, 103, 12, 32);
+    
+    if(mousePressed==true){
+     fill(#AAA3A4);{
+      ellipse(85,200,25,25); 
+     }
+}
+
+}
--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,32 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
-
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String momsBirthday = "February 1st";
+		String dadsBirthday = "July 12th";
+		String myBirthday = "November 28th";
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String answer = JOptionPane.showInputDialog("Which birthday would you like to know?");
 		// 3. Print out what the user typed
-		
+		System.out.println(answer);
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		if (answer.equalsIgnoreCase("mom")) {
+			System.out.println(momsBirthday);
+		}
 		// 5. if user asked for "dad"
-			// print dad's birthday
+		else if (answer.equalsIgnoreCase("dad")) {
+			System.out.println(dadsBirthday);
+		}
 		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else if (answer.equalsIgnoreCase("mine")) {
+			System.out.println(myBirthday);
+		}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else {
+		JOptionPane.showMessageDialog(null, "Sorry I don't remember that person's birthday!");
+		}
 
-	} 
+	}
 }
