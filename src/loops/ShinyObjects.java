de04430d43d98e1dab328930e4b2cc0ff566f958
pigchick3n <League@administorsimac.attlocal.net>
2018-05-26 11:27:45 -0700

    fizzBuzz(incomplete),Shiny Objects,and Walk of Fame

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,17 +5,22 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+           String number = JOptionPane.showInputDialog(null,"How many shiny objects do you want?");
 		// 3. Play the sound that many times
-
+           int times = Integer.parseInt(number);
+          for(int i=0;i<times;i++) {
+        	  playMisterZee();
+          }
+           
 		// 1. Call the method below
-
+         
 	}
 
 	public static void playMisterZee() {
--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,6 +3,7 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
 
 /**
 * Fizz Buzz
@@ -26,6 +27,14 @@ package modulo;
 **/
 
 public class FizzBuzz {
+public static void main(String[] args) {
+	for(int i=0;i<21;i++) {
+	System.out.println(i);
+	if(i%3==0) {
+	System.out.println("fizz");
+}
 
+}
+}
 }
 
